1fefc6fdeb24280edc8b7533c22f69fa202faa59
PODDFONAREM <91065141+poddfonarem@users.noreply.github.com>
2025-08-25 14:07:34 +0300

    Delete resources/views directory

--- resources/views/errors/404.php
+++
@@ -1,5 +0,0 @@
-<section class="py-5">
-    <div class="d-flex justify-content-center align-items-center flex-column text-center w-100">
-        <div class="e404-img"></div>
-    </div>
-</section>
--- resources/views/errors/500.php
+++
@@ -1,5 +0,0 @@
-<section class="py-5">
-    <div class="d-flex justify-content-center align-items-center flex-column text-center w-100">
-        <h1>ERROR: 500</h1>
-    </div>
-</section>
