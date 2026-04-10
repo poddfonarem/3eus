8850517dc3f5448fb102cd9543a3c3a3524e8685
PODDFONAREM <91065141+poddfonarem@users.noreply.github.com>
2026-04-10 10:21:54 +0300

    Add PDO::ATTR_EMULATE_PREPARES option to DB connection

--- app/Core/DB.php
+++ app/Core/DB.php
@@ -42,6 +42,7 @@ class DB
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_PERSISTENT         => true,
+                    PDO::ATTR_EMULATE_PREPARES   => false,
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
                 ]
             );
